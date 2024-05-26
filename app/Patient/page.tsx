5570f838e9418ee37d818556afd8adbb046d7a5b
Crosby patson <127304795+Efp-bsc-inf-03-20@users.noreply.github.com>
2024-05-26 21:59:18 +0200

    Update page.tsx

--- app/Patient/page.tsx
+++ app/Patient/page.tsx
@@ -7,17 +7,17 @@ const callouts = [
   {
     name: 'Patient Admission',
     description: 'Our hospitals patient admission service combines compassionate care with seamless efficiency. Our dedicated team prioritizes your comfort, safety, and well-being, providing personalized assistance with paperwork and expedited processing for a stress-free experience. Trust us to provide high-quality care from the moment you arrive.',
-    imageSrc: 'https://scontent.fblz1-1.fna.fbcdn.net/v/t39.30808-6/436570700_847620377384723_7356139790777712170_n.jpg?stp=dst-jpg_s640x640&_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEFxnS51F0QV5b2n5oA1GwSsHf1D8Gg7Uewd_UPwaDtR3HnadOEIpEyb3hNJ_E29CmIIJ3Y3fOuVa5Q_YNkQkXT&_nc_ohc=FjxYyF5ta3YQ7kNvgEu74fg&_nc_pt=5&_nc_zt=23&_nc_ht=scontent.fblz1-1.fna&oh=00_AYDfiFQq-pLAQFI8_I4Gz4l2S5ZXu_4_ctdxUIDXQZA8YA&oe=664C227D',
+    imageSrc: 'https://scontent.fblz2-1.fna.fbcdn.net/v/t39.30808-6/436570700_847620377384723_7356139790777712170_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEFxnS51F0QV5b2n5oA1GwSsHf1D8Gg7Uewd_UPwaDtR3HnadOEIpEyb3hNJ_E29CmIIJ3Y3fOuVa5Q_YNkQkXT&_nc_ohc=8K1N4ncYuroQ7kNvgEtpNow&_nc_zt=23&_nc_ht=scontent.fblz2-1.fna&oh=00_AYC-4dSnje4Mbcwnvgxq9CnvTou-aGGzRlDimcuIvKk-Qg&oe=6659517D',
   },
   {
     name: 'Patient Discharge',
     description: 'Experience a seamless departure from our hospital with our patient discharge service. As you prepare to leave, our compassionate team ensures a smooth transition, offering personalized support and guidance every step of the way. Rest assured, your comfort and well-being remain our top priority until the very end. Plus, as part of our commitment to your care, we provide the convenience of ambulance transport upon discharge, ensuring a safe journey home. Trust us to see you off with the same level of excellence and care you received during your stay.',
-    imageSrc: 'https://scontent.fblz1-1.fna.fbcdn.net/v/t39.30808-6/436233184_847621590717935_8368965591988716_n.jpg?stp=dst-jpg_p180x540&_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFg9w0tVPLCqv4jvnNuheHhUtqKLUzXVpdS2ootTNdWlxuH6ZJTnow5u0bsosoFjOHaxju26VaEuAaS6VlQDTf7&_nc_ohc=gmOq_Ipd-JsQ7kNvgHPVvvB&_nc_pt=5&_nc_zt=23&_nc_ht=scontent.fblz1-1.fna&oh=00_AYAUXWDg4kHcwKw3xL-h3hXITmdLMX4fsdmDEntwhIfa-g&oe=664C1DFB',
+    imageSrc: 'https://scontent.fblz2-1.fna.fbcdn.net/v/t39.30808-6/436233184_847621590717935_8368965591988716_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFg9w0tVPLCqv4jvnNuheHhUtqKLUzXVpdS2ootTNdWlxuH6ZJTnow5u0bsosoFjOHaxju26VaEuAaS6VlQDTf7&_nc_ohc=_GCN90eesp0Q7kNvgFDormg&_nc_zt=23&_nc_ht=scontent.fblz2-1.fna&oh=00_AYDHR3BLpWTgJMxPrHbdqwVYRUse8cJYd-MmRj2EkIzFkg&oe=66594CFB',
   },
   {
     name: 'Medical Checkup',
     description: 'Medical prescription guide from our medical specialist and as well as providing routine for check ups and any other related medical guide.',
-    imageSrc: 'https://scontent.fblz1-1.fna.fbcdn.net/v/t39.30808-6/440072618_847623114051116_5830858774568460928_n.jpg?stp=dst-jpg_p843x403&_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHlI4g6AqvMfoAH6x1NNSZz9AMN1EkRnOP0Aw3USRGc499nsOX6VkitwKv1ZzLZVsqpiAkkHQ7SL6GrrtBFMXn6&_nc_ohc=z1NbyF_VxMkQ7kNvgGUsBf2&_nc_pt=5&_nc_zt=23&_nc_ht=scontent.fblz1-1.fna&oh=00_AYCqrE5Ezb1yXrlKLEMFxsCH28zlsn8GYvzvLA6ZDgT2iw&oe=664C2703',
+    imageSrc: 'https://scontent.fblz2-1.fna.fbcdn.net/v/t39.30808-6/440072618_847623114051116_5830858774568460928_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHlI4g6AqvMfoAH6x1NNSZz9AMN1EkRnOP0Aw3USRGc499nsOX6VkitwKv1ZzLZVsqpiAkkHQ7SL6GrrtBFMXn6&_nc_ohc=7RUHqJv4Eh8Q7kNvgHgOOk3&_nc_zt=23&_nc_ht=scontent.fblz2-1.fna&oh=00_AYDJLXFwY3m8xBGmVPhcHPRBqj7cSswRS17v-2ydvLfmwQ&oe=66595603',
   },
 ];
 
